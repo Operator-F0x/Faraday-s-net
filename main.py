acfff0cb4c35ad7e5a15cb5da53f2c2311af82fc
F0x <Operator-F0x@proton.me>
2024-09-15 10:47:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,22 +30,25 @@ def main():
                               @@@@@""")
     
     print("""
-             1 Subnetting for ip + CIDR - (192.168.1.1/24)
-             2 Subnetting for ip + Subnet mask - (192.168.1.1 255.255.255.0)
-             3 calculate subnets from ip and desired subnets number - (192.168.1.1 5)
-             4 show binary octet of ip or subnet mask""")
+             1 Subnetting for IP + CIDR - (192.168.1.1/24)
+             2 Subnetting for IP + Subnet mask - (192.168.1.1 255.255.255.0)
+             3 Calculate subnets from IP and desired number of subnets - (192.168.1.1 5)
+             4 Show binary octet of IP or subnet mask""")
     
     function_chosen = input("--> ")
     if function_chosen == "1":
-        cidr = input("Enter the ip/CIDR -->  ")
+        cidr = input("Enter the IP/CIDR -->  ")
         subnet_info = calculate_subnet_from_cidr(cidr)
-        print(f"Network Address: {subnet_info['Network Address']}")
-        print(f"Broadcast Address: {subnet_info['Broadcast Address']}")
-        print(f"Usable Hosts Range: {subnet_info['Usable Hosts Range'][0]} to {subnet_info['Usable Hosts Range'][1]}")
+        print(f"""
+Network Address: {subnet_info['Network Address']}
+Broadcast Address: {subnet_info['Broadcast Address']}
+Usable Hosts Range: {subnet_info['Usable Hosts Range'][0]} to {subnet_info['Usable Hosts Range'][1]}
+""")
+
 
     if function_chosen == "3":
-        ip = input("Enter ip -->  ")
-        num_subnet = int(input("Enter number of subnet you want -->"))
+        ip = input("Enter IP -->  ")
+        num_subnet = int(input("Enter the number of subnets you want -->"))
 
         subnet_info = calculate_subnets(ip, num_subnet)
         
@@ -54,16 +57,22 @@ def main():
                 print(f"  Network Address: {info['Network Address']}")
                 print(f"  Subnet Mask: {info['Subnet Mask']}")
                 print(f"  Broadcast Address: {info['Broadcast Address']}")
-                print(f"  Gateway (primo host): {info['Gateway (primo host)']}")
+                print(f"  Gateway (first host): {info['Gateway (primo host)']}")
                 print(f"  Last Usable Host: {info['Last Usable Host']}")
                 print(f"  Total Usable Hosts: {info['Total Usable Hosts']}")
                 print()
 
-        subnet_number = int(input("Enter the subnet number you want details for --> "))
-        if 1 <= subnet_number <= len(subnet_info):
-            show_subnet_details(subnet_info[subnet_number - 1])
+        print("Want to see the details of a subnet?  - Y or N")
+        choice = input("--> ")
+        if choice == "Y":
+            subnet_number = int(input("Enter the subnet number you want details for --> "))
+            if 1 <= subnet_number <= len(subnet_info):
+                show_subnet_details(subnet_info[subnet_number - 1])
+            else:
+                print("Invalid subnet number!")
         else:
-            print("Invalid subnet number!")
+            return None            
+
 
 if __name__ == "__main__":
     main()
